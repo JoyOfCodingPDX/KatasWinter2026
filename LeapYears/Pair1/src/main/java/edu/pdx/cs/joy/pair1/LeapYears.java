17beda74bfba7204feff24ec77062ac4cc86d465
David Whitlock <david.m.whitlock@gmail.com>
2026-01-14 21:26:21 -0800

    Rename Kata class to LeapYears.

--- LeapYears/Pair1/src/main/java/edu/pdx/cs/joy/pair1/LeapYears.java
+++ LeapYears/Pair1/src/main/java/edu/pdx/cs/joy/pair1/LeapYears.java
@@ -2,13 +2,7 @@ package edu.pdx.cs.joy.pair1;
 
 import com.google.common.annotations.VisibleForTesting;
 
-/**
- * A class for getting started with a code kata
- *
- * Use IntelliJ's "Refactor | Rename..." command to change the name of this
- * class (and its tests).
- */
-public class Kata {
+public class LeapYears {
 
   @VisibleForTesting
   public static void main(String[] args) {
