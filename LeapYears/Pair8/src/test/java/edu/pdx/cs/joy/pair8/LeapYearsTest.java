63ddb0f91a9dcc3f4bdabc4e9ffc66d4b2324acf
David Whitlock <david.m.whitlock@gmail.com>
2026-01-14 22:08:20 -0800

    Add projects for remaining pairs.

---
+++ LeapYears/Pair8/src/test/java/edu/pdx/cs/joy/pair8/LeapYearsTest.java
@@ -0,0 +1,15 @@
+package edu.pdx.cs.joy.pair8;
+
+import org.junit.jupiter.api.Test;
+
+public class LeapYearsTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new LeapYears();
+  }
+
+}
+
+
