07bd0e4fb700832458189a982b852d5d72b3c543
David Whitlock <david.m.whitlock@gmail.com>
2026-01-14 19:15:35 -0800

    More FizzBuzz katas.

--- fizzbuzz/src/test/java/edu/pdx/cs/joy/pairs/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/edu/pdx/cs/joy/pairs/FizzBuzzTest.java
@@ -2,6 +2,9 @@ package edu.pdx.cs.joy.pairs;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class FizzBuzzTest
 {
 
@@ -9,5 +12,29 @@ public class FizzBuzzTest
   void canInstantiateKataClass() {
     new FizzBuzz();
   }
+
+  @Test
+  void fizzBuzzOf1Is1() {
+    int number = 1;
+    String fizzBuzzResult = "1";
+    assertFizzBuzz(number, fizzBuzzResult);
+  }
+
+  private static void assertFizzBuzz(int number, String fizzBuzzResult) {
+    String one = FizzBuzz.fizzBuzz(number);
+    assertThat(one, equalTo(fizzBuzzResult));
+  }
+
+  @Test
+  void fizzBuzzOf2Is2() {
+    assertFizzBuzz(2, "2");
+  }
+
+  @Test
+  void fizzBuzzOf3IsFizz() {
+    assertFizzBuzz(3, "Fizz");
+  }
+
+
 
 }
