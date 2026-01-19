2208a2e72366704335544d883a599191b9f8192b
David Whitlock <david.m.whitlock@gmail.com>
2026-01-19 07:57:54 -0800

    Added projects for the Lags kata.

---
+++ Lags/Pair2/src/test/java/edu/pdx/cs/joy/pair2/LagsTest.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs.joy.pair2;
+
+import org.junit.jupiter.api.Test;
+
+public class LagsTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new Lags();
+  }
+
+}
---
+++ Lags/Pair3/src/test/java/edu/pdx/cs/joy/pair3/LagsTest.java
@@ -0,0 +1,14 @@
+package edu.pdx.cs.joy.pair3;
+
+import org.junit.jupiter.api.Test;
+
+public class LagsTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new Lags();
+  }
+
+}
+
