d757d0934e74a838286b2dd3941b334a169bdecf
Adan Hernandez <adanh74379@gmail.com>
2026-01-21 18:31:28 -0800

    Got first test case passing

--- LeapYears/Pair8/src/main/java/edu/pdx/cs/joy/pair8/LeapYears.java
+++ LeapYears/Pair8/src/main/java/edu/pdx/cs/joy/pair8/LeapYears.java
@@ -8,4 +8,11 @@ public class LeapYears {
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
   }
+
+  public static String leapYears( int year ){
+    if (year % 400 == 0) {
+      return "Leap Year";
+    }
+    return "Not Leap Year";
+  }
 }
