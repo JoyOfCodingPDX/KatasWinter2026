9c7602aa970bfaa086f7e9ec3ad86d23b5d59ef1
DuhBeard <dylanisloco@gmail.com>
2026-01-21 18:43:49 -0800

    Group 11 for sure

--- LeapYears/Pair11/src/test/java/edu/pdx/cs/joy/pair11/LeapYearsTest.java
+++ LeapYears/Pair11/src/test/java/edu/pdx/cs/joy/pair11/LeapYearsTest.java
@@ -2,6 +2,9 @@ package edu.pdx.cs.joy.pair11;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class LeapYearsTest
 {
 
@@ -10,6 +13,16 @@ public class LeapYearsTest
     new LeapYears();
   }
 
+
+  @Test
+  private static void assertLeapYear(int number, String leapYearResult){
+    String one = LeapYears.leapYear(number);
+    assertThat(one, equalTo(leapYearResult));
+
+  }
+
 }
+
+
 
 
