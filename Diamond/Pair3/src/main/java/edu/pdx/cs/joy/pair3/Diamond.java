0ebc39306637458e868b67b2571543c03e9d7e83
jaysabe <jsabegglen96@gmail.com>
2026-01-28 18:48:51 -0800

    sdkjfgfdkj

--- Diamond/Pair3/src/main/java/edu/pdx/cs/joy/pair3/Diamond.java
+++ Diamond/Pair3/src/main/java/edu/pdx/cs/joy/pair3/Diamond.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class Diamond {
   private ArrayList<Character> letterList = new ArrayList<Character>();
 
-  public ArrayList<Character> getArrayFromChar(char letter) {
+  public ArrayList<Character> buildRow(char letter) {
     int length = (int) letter;
     length = (length - 65)*2 + 1;
     for(int i = 0; i < length; i++) {
@@ -21,9 +21,12 @@ public class Diamond {
   }
 
   public String diamondString(char letter) {
-    ArrayList<Character> list = getArrayFromChar(letter); // array of length
-    String result = "";
+    ArrayList<Character> list = buildRow(letter); // array of length
+    StringBuilder result = new StringBuilder();
     list.set(list.size() / 2, letter);
+    for (int i = 0; i < list.size() / 2; i++) {
+      //TBC
+    }
     return result;
   }
   @VisibleForTesting
