077778a34621f83952801ce5938e9e1f1af47cb3
Adan Hernandez <adanh74379@gmail.com>
2026-01-28 18:53:59 -0800

    Making more progress

--- Diamond/Pair5/src/main/java/edu/pdx/cs/joy/pair5/Diamond.java
+++ Diamond/Pair5/src/main/java/edu/pdx/cs/joy/pair5/Diamond.java
@@ -31,6 +31,19 @@ public class Diamond {
         System.out.println();
         currentLetter++;
       }
+      currentLetter -=2;
+      innerSpaces -=4;
+      for (int i = 1; i <= letter - 'A'; i++) {
+        printSpaces(i);
+        System.out.print(ALPHABET.charAt(currentLetter));
+        if (currentLetter != 0) {
+          printSpaces(innerSpaces);
+          innerSpaces-=2;
+          System.out.print(ALPHABET.charAt(currentLetter));
+        }
+        System.out.println();
+        currentLetter--;
+      }
     }
 //    System.out.println('C' - 'A');
 //    System.out.println('C' - 3);
