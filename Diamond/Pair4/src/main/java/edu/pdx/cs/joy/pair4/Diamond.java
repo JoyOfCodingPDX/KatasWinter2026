6eeed9b8f2de7312d5bd6b704227023b3fee19db
Jeremy <cuthbert@pdx.edu>
2026-01-28 18:36:08 -0800

    Updated diamond function

--- Diamond/Pair4/src/main/java/edu/pdx/cs/joy/pair4/Diamond.java
+++ Diamond/Pair4/src/main/java/edu/pdx/cs/joy/pair4/Diamond.java
@@ -6,30 +6,35 @@ public class Diamond {
 
   @VisibleForTesting
   public static void main(String[] args) {
-    cleanDiamonds('B');
+    cleanDiamonds('C');
     //System.err.println("Missing command line arguments");
   }
 
   public static void cleanDiamonds(char arg){
     int count = arg - 'A';
     int levels = count * 2 + 1;
-    char space = ' ';
 
     for (int i = 0; i <= count; i++){
       for (int j = count; j > i; j--){
           System.out.print(" ");
       }
-      System.out.print(arg - count + i);
+      System.out.print((char) (arg - count + i));
 
       for (int k = 0; k < (i * 2) - 1; k++) {
-        System.out.print(space);
+        System.out.print(" ");
       }
 
       if (i > 0){
-        System.out.print(arg - count + i);
+        System.out.print((char) (arg - count + i));
       }
-      //system.out.print(current_char);
-      //current_char += 1;
+      System.out.println();
+    }
+
+    for (int i = 0; i < count; i++) {
+      for (int j = 0; j <= i; j++) {
+        System.out.print(" ");
+      }
+      System.out.print((char)(arg - i - 1));
     }
   }
 
