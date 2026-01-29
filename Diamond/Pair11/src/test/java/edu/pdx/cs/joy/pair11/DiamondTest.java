8da6786e3ffe06bbf8ae16bb6ea324146ad16ed6
DuhBeard <dylanisloco@gmail.com>
2026-01-28 18:09:08 -0800

    Group 11 edits

--- Diamond/Pair11/src/test/java/edu/pdx/cs/joy/pair11/DiamondTest.java
+++ Diamond/Pair11/src/test/java/edu/pdx/cs/joy/pair11/DiamondTest.java
@@ -2,6 +2,8 @@ package edu.pdx.cs.joy.pair11;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DiamondTest
 {
 
@@ -10,6 +12,14 @@ public class DiamondTest
     new Diamond();
   }
 
+
+  @Test
+  void canPrintA(){
+    Diamond s = new Diamond();
+    String s = Diamond.printDiamond();
+    assertEquals("A", s)}
 }
+
+
 
 
