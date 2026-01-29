15dcb7a2b6f31f213a0c37cbe3a2c0f08b1f90a3
Shanice Uwase <uwase@ada.cs.pdx.edu>
2026-01-28 18:19:19 -0800

    First round done!

--- Diamond/Pair2/src/main/java/edu/pdx/cs/joy/pair2/Diamond.java
+++ Diamond/Pair2/src/main/java/edu/pdx/cs/joy/pair2/Diamond.java
@@ -1,19 +1,25 @@
 package edu.pdx.cs.joy.pair2;
 
 import com.google.common.annotations.VisibleForTesting;
+import java.util.ArrayList;
 
 public class Diamond {
 
-  private final String letters;
+  private final String letter;
+  private final ArrayList<>();
 
+  //constructor w/o args
   public Diamond()
   {
     this.letters = "";
   }
-  public Diamond(String letters)
+
+  //default constructor
+  public Diamond(String letter)
   {
-    this.letters = letters;
+    this.letter = letter;
   }
+
   @VisibleForTesting
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
