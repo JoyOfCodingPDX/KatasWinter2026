7dd6913e0327f795880ad2c8abd3b85e8a10723a
felipe <felipes@pdx.edu>
2026-01-28 18:03:53 -0800

    commiting

--- LeapYears/Pair4/src/test/java/edu/pdx/cs/joy/pair4/LeapYearsTest.java
+++ LeapYears/Pair4/src/test/java/edu/pdx/cs/joy/pair4/LeapYearsTest.java
@@ -2,15 +2,12 @@ package edu.pdx.cs.joy.pair4;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-<<<<<<< HEAD
+
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-=======
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
->>>>>>> 24ee2af0b8d3099b1eb9d737bd0ebc9fe307fc3a
+
 public class LeapYearsTest
 {
   @Test
@@ -18,7 +15,7 @@ public class LeapYearsTest
     new LeapYears();
   }
 
-<<<<<<< HEAD
+
   void is100aLeapYear() {
     int year = 100;
     boolean expected = false;
@@ -31,14 +28,8 @@ public class LeapYearsTest
   }
 
 }
-=======
-  @Test
-  void is100aleapyear() {
-    int year = 100;
-    int expected = 0;
-    assertThat(LeapYears.isLeapYear(year), equalTo(expected));  // Is that right?
-  }
->>>>>>> 24ee2af0b8d3099b1eb9d737bd0ebc9fe307fc3a
+
+
 
   @Disabled
   @Test
