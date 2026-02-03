b2d40961374c7b687712bf21ce44c0bd34498be7
David Whitlock <david.m.whitlock@gmail.com>
2026-02-02 18:14:39 -0800

    Started adding unit tests for the business logic of GildedRose.

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -16,4 +16,24 @@ class GildedRoseTest {
         assertEquals("fixme", app.items[0].name);
     }
 
+    @Test
+    void regularItemHasItsSellInAndQualityDecreasedByOneWhenUpdateQualityIsCalled() {
+        Item regularItem = new Item("Regular Item", 10, 20);
+        Item[] items = new Item[] {regularItem};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, regularItem.sellIn);
+        assertEquals(19, regularItem.quality);
+    }
+
+    @Test
+    void agedBrieIncreasesInQualityTheOlderItGets() {
+        Item agedBrie = new Item("Aged Brie", 2, 0);
+        Item[] items = new Item[]{agedBrie};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(1, agedBrie.sellIn);
+        assertEquals(1, agedBrie.quality);
+    }
+
 }
