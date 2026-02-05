625e02467e49e2c7274abf4c21d8e411f19a4c4b
AlexNovitchkovBurbank <ryzen71511@outlook.com>
2026-02-04 18:20:34 -0800

    Changes

--- Lags/Pair2/src/main/java/edu/pdx/cs/joy/pair2/Lags.java
+++ Lags/Pair2/src/main/java/edu/pdx/cs/joy/pair2/Lags.java
@@ -8,4 +8,8 @@ public class Lags {
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
   }
+
+  public static int getEndTimeFromStartTime(int start, int duration) {
+    return start + duration;
+  }
 }
