0eb4268aaa2afdc10a338fb00caf2f05f42adf2d
Stephen Feng <fengstep@cecs.pdx.edu>
2026-02-04 18:21:33 -0800

    Add flightHasConflict pair9

--- Lags/Pair9/src/main/java/edu/pdx/cs/joy/pair9/Lags.java
+++ Lags/Pair9/src/main/java/edu/pdx/cs/joy/pair9/Lags.java
@@ -3,13 +3,26 @@ package edu.pdx.cs.joy.pair9;
 import com.google.common.annotations.VisibleForTesting;
 
 public class Lags {
-
-  public Lags(String name, int start, int end, int profit) {
-
+  public String name;
+  public int start;
+  public int duration;
+  public int profit;
+  public Lags(String name, int start, int duration, int profit) {
+    this.name = name;
+    this.start = start;
+    this.duration = duration;
+    this.profit = profit;
   }
 
   @VisibleForTesting
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
+  }
+
+  public Boolean hasConflict(Lags other) {
+    if((this.start + this.duration) > (other.start)) {
+      return true;
+    }
+    return false;
   }
 }
--- Lags/Pair9/src/test/java/edu/pdx/cs/joy/pair9/LagsTest.java
+++ Lags/Pair9/src/test/java/edu/pdx/cs/joy/pair9/LagsTest.java
@@ -2,19 +2,21 @@ package edu.pdx.cs.joy.pair9;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class LagsTest
 {
-
   @Test
   void canInstantiateKataClass() {
     Lags flight1 = new Lags("AF514", 0, 5, 10);
   }
 
   @Test
-  void isConfict(){
-
+  void flightHasConflict() {
+    Lags flight1 = new Lags("AF514", 0, 5, 10);
+    Lags flight2 = new Lags("AF515", 3, 7, 14);
+    assertTrue(flight1.hasConflict(flight2));
   }
-
 }
 
 
