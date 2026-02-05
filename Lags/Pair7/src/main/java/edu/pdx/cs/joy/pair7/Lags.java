726bcb6883a52eb35cdfb970f42e68b033efd218
Jon Schmidt <schmid36@pdx.edu>
2026-02-04 18:21:04 -0800

    first round

--- Lags/Pair7/src/main/java/edu/pdx/cs/joy/pair7/Lags.java
+++ Lags/Pair7/src/main/java/edu/pdx/cs/joy/pair7/Lags.java
@@ -6,6 +6,18 @@ public class Lags {
 
   @VisibleForTesting
   public static void main(String[] args) {
+
+    String example = "afsdoafdsoafsd adfsh huafs dhadfs ih afsihafsd ";
+
+    if (args.length == 0) {
     System.err.println("Missing command line arguments");
+    }
+
+
+    for (String hello : args) {
+      System.out.println(hello);
+    }
+
+    return;
   }
 }
