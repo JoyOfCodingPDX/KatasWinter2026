2c1b926fc4e91ea7ac1121ef6c2861d56c57a59f
Ed Norris <4132634+ed-norris@users.noreply.github.com>
2026-02-04 18:20:52 -0800

    Initial version

--- Lags/Pair12/src/it/java/edu/pdx/cs/joy/pair12/LagsIT.java
+++ Lags/Pair12/src/it/java/edu/pdx/cs/joy/pair12/LagsIT.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LagsIT extends InvokeMainTestCase {
 
@@ -12,6 +13,12 @@ class LagsIT extends InvokeMainTestCase {
   void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
     InvokeMainTestCase.MainMethodResult result = invokeMain(Lags.class);
     assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+  }
+
+  @Test
+  void invokingMainWithMissingFileThrowsException() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Lags.class, "nonexistent.txt");
+    assertThat(result.getTextWrittenToStandardError(), containsString("missing file"));
   }
 
 
