af64cbdb7c9795e6242f88b7d29b12ea271f8f3e
David Whitlock <david.m.whitlock@gmail.com>
2026-02-09 18:37:11 -0800

    Introduce a switch statement for handling a BACKSTAGE_PASS.

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -24,39 +24,38 @@ class GildedRose {
             item.sellIn--;
             continue;
           }
+          case BACKSTAGE_PASS -> {
+            //- __"Backstage passes"__, like aged brie, increases in `Quality` as its `SellIn` value approaches;
+            //	- `Quality` increases by `2` when there are `10` days or less and by `3` when there are `5` days or less but
+            //	- `Quality` drops to `0` after the concert
+            if (item.sellIn <= 0) {
+              item.quality = 0;
+
+            } else if (item.quality < 50) {
+              int qualityImprovement;
+              if (item.sellIn <= 5) {
+                qualityImprovement = 3;
+              } else if (item.sellIn <= 10) {
+                qualityImprovement = 2;
+              } else {
+                qualityImprovement = 1;
+              }
+              item.quality = Math.min(item.quality + qualityImprovement, 50);
+            }
+            item.sellIn--;
+            continue;
+          }
         }
 
-        if (!item.name.equals(BACKSTAGE_PASS)) {
-          if (item.quality > 0) {
-            item.quality--;
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality++;
-
-            if (item.sellIn <= 10) {
-              if (item.quality < 50) {
-                item.quality++;
-              }
-            }
-
-            if (item.sellIn <= 5) {
-              if (item.quality < 50) {
-                item.quality++;
-              }
-            }
-          }
+        if (item.quality > 0) {
+          item.quality--;
         }
 
         item.sellIn--;
 
         if (item.sellIn < 0) {
-          if (!item.name.equals(BACKSTAGE_PASS)) {
-            if (item.quality > 0) {
-              item.quality--;
-            }
-          } else {
-            item.quality = 0;
+          if (item.quality > 0) {
+            item.quality--;
           }
         }
       }
