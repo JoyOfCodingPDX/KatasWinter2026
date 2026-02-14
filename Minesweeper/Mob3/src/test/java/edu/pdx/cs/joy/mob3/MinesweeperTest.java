a6d53b10405440d9cbbd8fb0a44508f072f2b9d7
David Whitlock <david.m.whitlock@gmail.com>
2026-02-14 08:28:41 -0800

    Added Minesweeper katas.

---
+++ Minesweeper/Mob3/src/test/java/edu/pdx/cs/joy/mob3/MinesweeperTest.java
@@ -0,0 +1,14 @@
+package edu.pdx.cs.joy.mob3;
+
+import org.junit.jupiter.api.Test;
+
+public class MinesweeperTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new Minesweeper();
+  }
+
+}
+
---
+++ Minesweeper/Mob4/src/test/java/edu/pdx/cs/joy/mob4/MinesweeperTest.java
@@ -0,0 +1,14 @@
+package edu.pdx.cs.joy.mob4;
+
+import org.junit.jupiter.api.Test;
+
+public class MinesweeperTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new Minesweeper();
+  }
+
+}
+
