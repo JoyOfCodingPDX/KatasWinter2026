4ada4e073f7335739e0fe4f87b4eaf9a2d36abcd
jaysabe <jsabegglen96@gmail.com>
2026-02-16 18:28:03 -0800

    init commit

--- Tennis/src/main/java/edu/pdx/cs/joy/mob/Tennis.java
+++ Tennis/src/main/java/edu/pdx/cs/joy/mob/Tennis.java
@@ -9,6 +9,21 @@ import com.google.common.annotations.VisibleForTesting;
  * class (and its tests).
  */
 public class Tennis {
+  private final String name1;
+  private final String name2;
+
+  public Tennis(String name1, String name2) {
+    this.name1 = name1;
+    this.name2 = name2;
+  }
+
+  public String getPlayerOne() {
+    return name1;
+  }
+
+  public String getPlayerTwo() {
+    return name2;
+  }
 
   @VisibleForTesting
   public static void main(String[] args) {
--- Tennis/src/test/java/edu/pdx/cs/joy/mob/TennisTest.java
+++ Tennis/src/test/java/edu/pdx/cs/joy/mob/TennisTest.java
@@ -2,6 +2,9 @@ package edu.pdx.cs.joy.mob;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
 public class TennisTest
 {
 
@@ -10,4 +13,10 @@ public class TennisTest
     new Tennis();
   }
 
+  @Test
+  void canNameInitPlayersTest(){
+    Tennis tennis = new Tennis("Name1", "Name2");
+    String player1 = tennis.getPlayerOne();
+    assertThat(player1, containsString("Name1"));
+  }
 }
