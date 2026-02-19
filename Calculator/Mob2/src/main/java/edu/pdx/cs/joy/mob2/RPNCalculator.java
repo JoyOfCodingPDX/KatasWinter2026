a338b4d9cd3b6ed997ea13305160ce7ec2d04940
Kaitlyn <harvey29@pdx.edu>
2026-02-18 18:17:43 -0800

    1st round

--- Calculator/Mob2/src/main/java/edu/pdx/cs/joy/mob2/RPNCalculator.java
+++ Calculator/Mob2/src/main/java/edu/pdx/cs/joy/mob2/RPNCalculator.java
@@ -10,8 +10,19 @@ import com.google.common.annotations.VisibleForTesting;
  */
 public class RPNCalculator {
 
+  //private static final Set<String> VALID_OPTIONS = "";
   @VisibleForTesting
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
+  }
+
+  /**
+   *
+   * @param s
+   * @return result
+   */
+  public double calculate(String s) {
+    //String input[] = s.split(" ");
+    return 4.0;
   }
 }
--- Calculator/Mob2/src/test/java/edu/pdx/cs/joy/mob2/RPNCalculatorTest.java
+++ Calculator/Mob2/src/test/java/edu/pdx/cs/joy/mob2/RPNCalculatorTest.java
@@ -2,6 +2,9 @@ package edu.pdx.cs.joy.mob2;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class RPNCalculatorTest
 {
 
@@ -10,5 +13,12 @@ public class RPNCalculatorTest
     new RPNCalculator();
   }
 
+  @Test
+  void canCalculate() {
+    RPNCalculator newCalc = new RPNCalculator();
+
+    double result = newCalc.calculate("2 2 +");
+    assertThat(result, equalTo(4.0));
+  }
 }
 
