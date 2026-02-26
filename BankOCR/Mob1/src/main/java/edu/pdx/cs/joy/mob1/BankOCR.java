d9c9efe3fe089fd56459d2d7f839254ffd4b1ab4
Gamaliel Rojas <jarib042689@gmail.com>
2026-02-25 18:55:29 -0800

    psuh

--- BankOCR/Mob1/src/main/java/edu/pdx/cs/joy/mob1/BankOCR.java
+++ BankOCR/Mob1/src/main/java/edu/pdx/cs/joy/mob1/BankOCR.java
@@ -10,11 +10,21 @@ import com.google.common.annotations.VisibleForTesting;
  */
 public class BankOCR {
 
+
+  private final Reader reader;
+  public TextParser(Reader reader){
+    this.reader = reader;
+  }
+
   @VisibleForTesting
   public static void main(String[] args) {
     if(args.length == 1){
       System.out.println(args[0]);
-
+      BufferedReader br = new BufferedReader(this.reader);
+      String line1 = br.readline();
+      String line2 = br.readline();
+      String line3 = br.readline();
+      String line4 = br.readline();
     }else{
       System.err.println("Invalid command line arguments");
     }
