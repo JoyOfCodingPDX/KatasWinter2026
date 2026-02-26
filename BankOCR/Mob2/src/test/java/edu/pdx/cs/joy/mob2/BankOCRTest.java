fdc69e8e4d35bbf04e1571e9f81d821036694adb
Adan Hernandez <adanh74379@gmail.com>
2026-02-25 18:43:01 -0800

    Update Code

--- BankOCR/Mob2/src/test/java/edu/pdx/cs/joy/mob2/BankOCRTest.java
+++ BankOCR/Mob2/src/test/java/edu/pdx/cs/joy/mob2/BankOCRTest.java
@@ -23,12 +23,33 @@ public class BankOCRTest
   }
 
   @Test
-  void canParseOneNumber() {
+  void canParseZeroThroughNine() {
     BankOCR bankOCR = new BankOCR();
 
     int parsedOne = bankOCR.parse(BankOCR.one);
+    int parsedTwo = bankOCR.parse(BankOCR.two);
+    int parsedThree = bankOCR.parse(BankOCR.thr);
+    int parsedFour = bankOCR.parse(BankOCR.fou);
+    int parsedFive = bankOCR.parse(BankOCR.fiv);
+    int parsedSix = bankOCR.parse(BankOCR.six);
+    int parsedSeven = bankOCR.parse(BankOCR.sev);
+    int parsedEight = bankOCR.parse(BankOCR.eig);
+    int parsedNine = bankOCR.parse(BankOCR.nin);
 
     assertThat(parsedOne, equalTo(1));
+    assertThat(parsedTwo, equalTo(2));
+    assertThat(parsedThree, equalTo(3));
+    assertThat(parsedFour, equalTo(4));
+    assertThat(parsedFive, equalTo(5));
+    assertThat(parsedSix, equalTo(6));
+    assertThat(parsedSeven, equalTo(7));
+    assertThat(parsedEight, equalTo(8));
+    assertThat(parsedNine, equalTo(9));
   }
+
+//  @Test
+//  void canParseNumberTwo() {
+//    BankOCR bankOCR = new
+//  }
 
 }
