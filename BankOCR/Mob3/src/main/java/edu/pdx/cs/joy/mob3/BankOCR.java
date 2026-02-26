450b7c677b1bf554e5921bc9525f203e85d79194
Jon Schmidt <schmid36@pdx.edu>
2026-02-25 18:57:05 -0800

    attempt commit

--- BankOCR/Mob3/src/main/java/edu/pdx/cs/joy/mob3/BankOCR.java
+++ BankOCR/Mob3/src/main/java/edu/pdx/cs/joy/mob3/BankOCR.java
@@ -10,11 +10,37 @@ import com.google.common.annotations.VisibleForTesting;
  */
 public class BankOCR {
 
+  static String zero = " _ | ||_|";
+
   @VisibleForTesting
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
   }
   public static String parse(String str1, String str2, String str3){
 
+    String toReturn = "";
+
+    //for loop
+    for (int i = 0; i < 27; i += 3) {
+
+      String temp = "";
+      temp += str1.charAt(i);
+      temp += str1.charAt(i+1);
+      temp += str1.charAt(i+2);
+
+      temp += str2.charAt(i);
+      temp += str2.charAt(i+1);
+      temp += str2.charAt(i+2);
+
+      temp += str3.charAt(i);
+      temp += str3.charAt(i+1);
+      temp += str3.charAt(i+2);
+
+      if (temp.equals(zero)) {
+        toReturn += "0";
+      }
+    }
+
+    return toReturn;
   }
 }
