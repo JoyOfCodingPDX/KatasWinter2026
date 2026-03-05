c84ed97de7cf34697f2951327dc7826d91ba9beb
Adan Hernandez <adanh74379@gmail.com>
2026-03-04 18:12:32 -0800

    Round 1

--- Minesweeper/Mob1/src/main/java/edu/pdx/cs/joy/mob1/Minesweeper.java
+++ Minesweeper/Mob1/src/main/java/edu/pdx/cs/joy/mob1/Minesweeper.java
@@ -10,8 +10,34 @@ import com.google.common.annotations.VisibleForTesting;
  */
 public class Minesweeper {
 
+  public Minesweeper() {
+
+  }
+
   @VisibleForTesting
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
   }
+
+  public String[][] sweep(String[][] mineField){
+    int numberOfRows = mineField.length;
+    String[][] result = new String[numberOfRows][];
+    for (int i = 0; i < numberOfRows; i++){
+      for (int j = 0; j < mineField[i].length; j++){
+
+      }
+    }
+  }
+
+  public String transmit(String[][] mineField, int row, int col){
+    String topLeft;
+    String top;
+    String topRight;
+  }
+  /*
+  *...
+  ....
+  ..*.
+  *...
+   */
 }
